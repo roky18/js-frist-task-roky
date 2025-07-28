7e2d0b24cea242330de93423b158ca70a1678827
roky18 <rokymax66@gmail.com>
2025-07-28 20:55:51 +0600

    2nd update

---
+++ BMI.js
@@ -0,0 +1,13 @@
+// BMI Calculator and Helth Category
+
+const BMI = 30;
+
+if (BMI < 18.5) {
+  console.log("you are underweight.");
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+  console.log("you are normal.");
+} else if (BMI >= 25 && BMI <= 29.9) {
+  console.log("you are overweight.");
+} else {
+  console.log("you are not perfect man");
+}
---
+++ free-drinks.js
@@ -0,0 +1,7 @@
+// Free Drinks
+const BurgerPrice = 500;
+if (BurgerPrice >= 500) {
+  console.log("congratulation, you win a free coke");
+} else {
+  console.log("please paid 30tk for coke");
+}
